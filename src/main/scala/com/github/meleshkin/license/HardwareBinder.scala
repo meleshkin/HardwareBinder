e8771e242fddcf42d6bbbfa4cdd29aaaccf1931c
meleshkin <mikhail.meleshkin@gmail.com>
2024-08-02 21:22:30 +0300

    added andThen

--- src/main/scala/com/github/meleshkin/license/HardwareBinder.scala
+++ src/main/scala/com/github/meleshkin/license/HardwareBinder.scala
@@ -1,32 +1,39 @@
 package com.github.meleshkin.license
 
-object HardwareBinder extends App{
-  type ABSTRACT_HW[S, H] = (State[S], HAL[H]) => State[S]
-  type HW = ABSTRACT_HW[String, State[String]]
+import com.github.meleshkin.license.HardwareBinder.ABSTRACT_HW
 
-  val st1: HW = (s, h) => h.updateWithCpuInfo(s)
-  val st2: HW = (s, h) => h.updateWithGraphicCards(s)
+object HardwareBinder extends App{
+  type ABSTRACT_HW[SA, SB, H] = (State[SA], HAL[H]) => State[SB]
+  type HW = ABSTRACT_HW[String, String, State[String]]
+
+  val st1:ABSTRACT_HW[String, String, State[String]] = (s, h) => h.updateWithCpuInfo(s)
+  val st2: ABSTRACT_HW[String, String, State[String]]  = (s, h) => h.updateWithGraphicCards(s)
 
   val h: HAL[State[String]] = new MockHal
   val s: State[String] = State(Nil)
 
 
-  def run[A, B](f: ABSTRACT_HW[A, B])(s: State[A], h: HAL[B]) = {
+  def run[SA, SB, H](f: ABSTRACT_HW[SA, SB, H])(s: State[SA], h: HAL[H]) = {
     f(s, h)
   }
-
+  
   run(st1)(s, h)
+    .andThen(st2)(h)
 }
 
 case class State[+A](get: List[A]) {
   def update[B >: A](b: B): State[B] = {
     State(b :: get)
   }
+
+  def andThen[B](f: ABSTRACT_HW[A, B, State[B]])(h: HAL[State[B]]): State[B] = {
+    f(this, h)
+  }
+
 }
 case object State {
   def apply[A](get: List[A]): State[A] = new State[A](get)
 }
-
 
 trait HAL[S] {
   def updateWithCpuInfo(state: S): S
@@ -38,12 +45,12 @@ class MockHal extends HAL[State[String]] {
   override def updateWithCpuInfo(state: State[String]): State[String] = {
     println("Update With Cpu Info")
     val s: String = "Some cpu info"
-    State(s :: Nil)
+    state.update(s)
   }
 
   override def updateWithGraphicCards(state: State[String]): State[String] = {
     println("Update With Graphic cards")
     val s: String = "Some videocard"
-    State(s :: Nil)
+    state.update(s)
   }
 }
